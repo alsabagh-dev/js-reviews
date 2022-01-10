b6851adf94166750d885b941718307a07e303a4d
Abdelaziz Alsabagh <dev.alsabagh@gmail.com>
2022-01-10 16:59:30 +0200

    add show person

--- app.js
+++ app.js
@@ -37,3 +37,52 @@ const reviews = [
       "Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ",
   },
 ];
+
+// Select items
+const  img  = document.getElementById('person-img');
+const  author  = document.getElementById('author');
+const  job  = document.getElementById('job');
+const  info  = document.getElementById('info');
+
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const randomBtn = document.querySelector('.random-btn');
+
+// Set starting item
+let currentItem = 0;
+
+// show person based on item
+const showPerson = (person) => {
+  const item = reviews[person];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// load initial
+window.addEventListener('DOMContentLoaded', () => {
+  showPerson(currentItem);
+});
+
+// show next person
+nextBtn.addEventListener('click',  () => {
+  currentItem += 1;
+  if (currentItem > reviews.length - 1) {
+    currentItem = 0;
+  }
+  showPerson(currentItem);
+});
+// show prev person
+prevBtn.addEventListener('click',  () => {
+  currentItem -= 1;
+  if (currentItem < 0) {
+    currentItem = reviews.length - 1;
+  }
+  showPerson(currentItem);
+});
+// show random person
+randomBtn.addEventListener('click',  () => {
+  currentItem = Math.floor(Math.random() * reviews.length);
+  showPerson(currentItem);
+});
